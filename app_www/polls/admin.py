70139332b245fec8293f33b19a95d9b73105ebf2
Daniel <d.mlynarczyk16@gmail.com>
2022-10-22 22:58:02 +0200

    register model Druzyna in admin panel

--- app_www/polls/admin.py
+++ app_www/polls/admin.py
@@ -3,10 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 from .models import Osoba
+from .models import Druzyna
 
 
 class OsobaAdmin(admin.ModelAdmin):
     list_display = ['imie', 'nazwisko', 'miesiac_urodzenia', 'data_dodania']
 
+class DruzynaAdmin(admin.ModelAdmin):
+    list_display = ['nazwa', 'kraj']
+
 admin.site.register(Question)
 admin.site.register(Osoba, OsobaAdmin)
+admin.site.register(Druzyna)
