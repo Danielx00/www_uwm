237e7ac71c93305ff80db83213e6553f5c54b3e9
Daniel <d.mlynarczyk16@gmail.com>
2022-10-22 22:37:22 +0200

    display another columns in admin panel

--- app_www/polls/admin.py
+++ app_www/polls/admin.py
@@ -4,5 +4,9 @@ from django.contrib import admin
 from .models import Question
 from .models import Osoba
 
+
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko', 'miesiac_urodzenia', 'data_dodania']
+
 admin.site.register(Question)
-admin.site.register(Osoba)
+admin.site.register(Osoba, OsobaAdmin)
