a42c8700a9444589f8a3f8f0c1543cd80c4a931b
Daniel <d.mlynarczyk16@gmail.com>
2022-10-23 16:19:36 +0200

    add filters

--- app_www/polls/admin.py
+++ app_www/polls/admin.py
@@ -8,10 +8,11 @@ from .models import Druzyna
 
 class OsobaAdmin(admin.ModelAdmin):
     list_display = ['imie', 'nazwisko', 'miesiac_urodzenia', 'data_dodania', 'druzyna']
-    list_filter = ['druzyna']
+    list_filter = ['druzyna', 'data_dodania']
 
 class DruzynaAdmin(admin.ModelAdmin):
     list_display = ['nazwa', 'kraj']
+    list_filter = ['kraj']
 
 admin.site.register(Question)
 admin.site.register(Osoba, OsobaAdmin)
