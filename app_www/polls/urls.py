70540bfa2c45ac84eda559e66fba876475547816
Daniel Młynarczyk <d.mlynarczyk16@gmail.com>
2022-11-11 16:42:20 +0100

    implement endpoints for osoba using classes view (APIView)

--- app_www/polls/urls.py
+++ app_www/polls/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('persons/', views.person_list),
-    path('persons/<int:pk>/', views.person_detail),
+    path('persons/', views.PersonsList.as_view()),
+    path('persons/<int:pk>/', views.PersonsList.as_view()),
     path('teams/', views.team_list),
     path('teams/<int:pk>/', views.team_detail),
 
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- app_www/polls/views.py
+++ app_www/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Osoba, Druzyna
 from .serializers import OsobaModelSerializer, DruzynaModelSerializer
@@ -8,35 +8,36 @@ from .serializers import OsobaModelSerializer, DruzynaModelSerializer
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-@api_view(['GET'])
-def person_list(request):
 
-    if request.method == 'GET':
+class PersonsList(APIView):
+
+    def get(self, request, format=None):
         persons = Osoba.objects.all()
         serializer = OsobaModelSerializer(persons, many=True)
         return Response(serializer.data)
 
+class PersonDetail(APIView):
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def person_detail(request, pk):
-    try:
-        person = Osoba.objects.get(pk=pk)
-    except Osoba.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            person = Osoba.objects.get(pk=pk)
+        except Osoba.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
         person = Osoba.objects.get(pk=pk)
         serializer = OsobaModelSerializer(person)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = OsobaModelSerializer(person, data=request.data)
+    def put(self, request, pk, format=None):
+        serializer = OsobaModelSerializer(self.get_object(pk), data=request.data)
         if(serializer.is_valid()):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        person = self.get_object(pk)
         person.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
