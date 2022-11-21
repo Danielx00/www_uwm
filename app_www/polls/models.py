01e9381b30c85589c815f2f4acd7169237ba4f01
Daniel Młynarczyk <d.mlynarczyk16@gmail.com>
2022-11-21 22:57:04 +0100

    add receiver to Osoba model

--- app_www/polls/models.py
+++ app_www/polls/models.py
@@ -2,6 +2,10 @@ from django.db import models
 import datetime
 from django.utils import timezone
 from datetime import date
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 class Question(models.Model):
     question_text = models.CharField(max_length=150)
@@ -65,6 +69,11 @@ class Osoba(models.Model):
 
     def __str__(self):
         return self.imie + ' ' + self.nazwisko
+
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+            Token.objects.create(user=instance)
 
 
 
